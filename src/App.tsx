7fc260ec4a332e3bb74cd609eff58eeec5dc397d
Linda Loftsgarden <linda-fintlabs@users.noreply.github.com>
2023-05-01 22:17:41 +0200

    removed createStyle, makeStyle, and added files for theme and styles

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,9 @@
 import React from 'react';
-import {createTheme} from '@mui/material/styles';
 import {ThemeProvider} from "@mui/styles";
 import {Route, Routes} from 'react-router-dom';
 import Main from './main/Main'
 import MenuProvider from "./context/menuContext";
-
-const theme = createTheme({
-    palette: {
-        secondary: {
-            light: '#7fb434',
-            main: '#5FA202',
-            dark: '#427101',
-        },
-        primary: {
-            light: '#4b727a',
-            main: '#1F4F59',
-            dark: '#15373e',
-        },
-    },
-    spacing: 8,
-    typography: {
-        fontFamily: [
-            "Nunito Sans", 'sans-serif'
-        ].join(',')
-    },
-});
+import theme from "./template/theme";
 
 function App() {
     return (
---
+++ src/template/style.tsx
@@ -0,0 +1,10 @@
+const drawerWidth = 240;
+
+const style = {
+    drawer: {
+        width: drawerWidth,
+        [`& .MuiDrawer-paper`]: {width: drawerWidth}
+    },
+}
+
+export default style;
